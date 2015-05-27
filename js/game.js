dafbf7ca18e5bcc403b9d4137370c05b3ee7d484
Emma Berglund <emma.berglund@live.se>
2015-05-27 15:59:28 +0200

    uppdaterade med en counter-funktion, antal gånger kvar att gissa

--- js/game.js
+++ js/game.js
@@ -7,32 +7,55 @@ var valtOrd = ordlista[Math.floor(Math.random()*3)];
 console.log(valtOrd);
 
 
+var tries = 0;
+var counts = 3;
+
+
 /*Låt användaren gissa ordet */
 
 var button = document.getElementsByTagName("button")[0];
-button.addEventListener("click", function () {
-    var anvandarOrd = document.getElementsByTagName("input")[0].value;
-    
-    /* Hämta rutor och lägg in bokstäverna rätt */
-    
-    var rutor = document.getElementsByTagName("td");
-    
-    for(var i= 0; i < 5; i++){
-        rutor[i].innerHTML = anvandarOrd[i];
-    
-        if(anvandarOrd[i]===valtOrd[i]){
-        rutor[i].style.background = "green";
+
+   
+    button.addEventListener("click", function () {
         
-    
-        }
-        else {
-            rutor[i].style.background = "red";
+        if(tries<counts){
+            tries++;
+            
+        var anvandarOrd = document.getElementsByTagName("input")[0].value;
+
+        /* Hämta rutor och lägg in bokstäverna rätt */
+
+        var rutor = document.getElementsByTagName("td");
+
+        for(var i= 0; i < 5; i++){
+            rutor[i].innerHTML = anvandarOrd[i];
+
+            if(anvandarOrd[i]===valtOrd[i]){
+            rutor[i].style.background = "green";
+
+
+            }
+            else {
+                rutor[i].style.background = "red";
+
+            }
+
 
         }
+        var timesLeft = counts-tries;
 
-    
-    }
-    
+        if(anvandarOrd===valtOrd){
+            alert("GRATTIS! DU VANN!");
+        }
+        else if(tries<counts){
+            alert("FÖRSÖK IGEN! Du har nu " + timesLeft + " gånger kvar att gissa.");          
+        }
+        else if (tries>=counts){
+            alert("DU FÖRLORADE! DIN SOPA!")
+            
+        }
     
 
-})
+  
+}       })
+      
